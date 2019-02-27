77e5ad35b387115b70164e35c158430a383cd879
jeankillian <jeankillian@yahoo.fr>
2019-02-27 16:33:38 +0100

    first upload

--- Stats.py
+++ Stats.py
@@ -1,5 +1,20 @@
-def main():
-    print("Hello World")
+import os
+import json
 
+
+def main():
+    table_parties = []
+    for partie in os.listdir('./Partie'):
+        with open('./Partie/' + partie, 'r') as fichier:
+            stats = json.loads(fichier.read())
+            print(stats)
+            table_parties.append(stats)
+    print(table_parties)
+
+
+def stats_jour():
+    compteur = 0
+    
 
 main()
+
